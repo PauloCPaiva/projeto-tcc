1744d037271564af3c97d83a99895a6d2970febf
PauloCPaiva <paulocesarapjr@gmail.com>
2025-04-08 19:04:11 -0300

    primeiro commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styles from "../Header/Header.module.css";
+import LogoHeader from "../../assets/img/logo.svg";
+import iconCloser from "../../assets/img/menu-closer.svg";
+import iconBars from "../../assets/img/menu-togle.svg";
+
+const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+  return (
+    <header className={`${styles.Header} ${menuActive ? styles.active : ""}`}>
+      <div className={styles.wrapperHeader}>
+        <div>
+          <button>
+            <img src={LogoHeader} alt="" />
+          </button>
+        </div>
+        <button onClick={toggleMenu} id={styles.buttonMobile}>
+          <img
+            src={menuActive ? iconCloser : iconBars}
+            id={styles.menuIcons}
+            alt=""
+          />
+        </button>
+        <nav className={menuActive ? styles.active : ""}>
+          <ul className={styles.ulMenu}>
+            <li className={styles.navLi}>
+              <a href="">Apresentação</a>
+            </li>
+            <li className={styles.navLi}>
+              <a href="">Cursos</a>
+            </li>
+            <li className={styles.navLi}>
+              <a href="">Galeria</a>
+            </li>
+            <li className={styles.navLi}>
+              <a href="">Novos</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
